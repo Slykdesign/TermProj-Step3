875ec95051c001f4b0d521b37847d3a165a6a176
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-07 14:08:23 +0000

    Update ext2.h

--- ext2.h
+++ ext2.h
@@ -1,13 +1,11 @@
 #ifndef EXT2_H
 #define EXT2_H
 
-#include "vdi.h"
 #include "partition.h"
 #include <stdint.h>
 #include <stdbool.h>
 
-// Ext2 Superblock structure
-struct Ext2Superblock {
+typedef struct {
     uint32_t s_inodes_count;
     uint32_t s_blocks_count;
     uint32_t s_r_blocks_count;
@@ -15,14 +13,14 @@ struct Ext2Superblock {
     uint32_t s_free_inodes_count;
     uint32_t s_first_data_block;
     uint32_t s_log_block_size;
-    int32_t s_log_frag_size;
+    uint32_t s_log_frag_size;
     uint32_t s_blocks_per_group;
     uint32_t s_frags_per_group;
     uint32_t s_inodes_per_group;
     uint32_t s_mtime;
     uint32_t s_wtime;
     uint16_t s_mnt_count;
-    int16_t s_max_mnt_count;
+    uint16_t s_max_mnt_count;
     uint16_t s_magic;
     uint16_t s_state;
     uint16_t s_errors;
@@ -39,28 +37,27 @@ struct Ext2Superblock {
     uint32_t s_feature_compat;
     uint32_t s_feature_incompat;
     uint32_t s_feature_ro_compat;
-    uint8_t s_uuid[16];
-    char s_volume_name[16];
-    char s_last_mounted[64];
+    uint8_t  s_uuid[16];
+    char     s_volume_name[16];
+    char     s_last_mounted[64];
     uint32_t s_algorithm_usage_bitmap;
-    uint8_t s_prealloc_blocks;
-    uint8_t s_prealloc_dir_blocks;
+    uint8_t  s_prealloc_blocks;
+    uint8_t  s_prealloc_dir_blocks;
     uint16_t s_padding1;
-    uint8_t s_journal_uuid[16];
+    uint8_t  s_journal_uuid[16];
     uint32_t s_journal_inum;
     uint32_t s_journal_dev;
     uint32_t s_last_orphan;
     uint32_t s_hash_seed[4];
-    uint8_t s_def_hash_version;
-    uint8_t s_reserved_char_pad;
+    uint8_t  s_def_hash_version;
+    uint8_t  s_reserved_char_pad;
     uint16_t s_reserved_word_pad;
     uint32_t s_default_mount_opts;
     uint32_t s_first_meta_bg;
     uint32_t s_reserved[190];
-};
+} Ext2Superblock;
 
-// Ext2 Block Group Descriptor structure
-struct Ext2BlockGroupDescriptor {
+typedef struct {
     uint32_t bg_block_bitmap;
     uint32_t bg_inode_bitmap;
     uint32_t bg_inode_table;
@@ -69,25 +66,25 @@ struct Ext2BlockGroupDescriptor {
     uint16_t bg_used_dirs_count;
     uint16_t bg_pad;
     uint32_t bg_reserved[3];
-};
+} Ext2BlockGroupDescriptor;
 
-// Ext2 File structure
-struct Ext2File {
-    VDIFile *vdi;
+typedef struct {
     MBRPartition *partition;
-    struct Ext2Superblock superblock;
-    struct Ext2BlockGroupDescriptor *bgdt;
+    Ext2Superblock superblock;
+    Ext2BlockGroupDescriptor *bgdt;
     uint32_t block_size;
     uint32_t num_block_groups;
-};
+} Ext2File;
 
-struct Ext2File *ext2Open(char *fn);
-void ext2Close(struct Ext2File *f);
-bool fetchBlock(struct Ext2File *f, uint32_t blockNum, void *buf);
-bool writeBlock(struct Ext2File *f, uint32_t blockNum, void *buf);
-bool fetchSuperblock(struct Ext2File *f, uint32_t blockNum, struct Ext2Superblock *sb);
-bool writeSuperblock(struct Ext2File *f, uint32_t blockNum, struct Ext2Superblock *sb);
-bool fetchBGDT(struct Ext2File *f, uint32_t blockNum, struct Ext2BlockGroupDescriptor *bgdt);
-bool writeBGDT(struct Ext2File *f, uint32_t blockNum, struct Ext2BlockGroupDescriptor *bgdt);
+Ext2File *openExt2File(const char *filename);
+void closeExt2File(Ext2File *f);
+bool fetchBlock(Ext2File *f, uint32_t blockNum, void *buf);
+bool writeBlock(Ext2File *f, uint32_t blockNum, void *buf);
+bool fetchSuperblock(Ext2File *f, uint32_t blockNum, Ext2Superblock *sb);
+bool writeSuperblock(Ext2File *f, uint32_t blockNum, Ext2Superblock *sb);
+bool fetchBGDT(Ext2File *f, uint32_t blockNum, Ext2BlockGroupDescriptor *bgdt);
+bool writeBGDT(Ext2File *f, uint32_t blockNum, Ext2BlockGroupDescriptor *bgdt);
+void displaySuperblock(Ext2Superblock *sb);
+void displayBlockGroupDescriptorTable(Ext2BlockGroupDescriptor *bgdt, uint32_t num_block_groups);
 
 #endif
